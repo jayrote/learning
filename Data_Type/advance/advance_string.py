a89ea74b77e386a59be5ea9ec7fbcc32cae6911d
jayrote <80813964+jayrote@users.noreply.github.com>
2021-03-18 16:02:13 +0530

    examples

---
+++ Data_Type/advance/advance_string.py
@@ -0,0 +1,27 @@
+# 1.
+
+a = 4
+
+b = 4.5
+
+c = a + b
+
+print(f" THE ANSWER OF SUM IS = {c}")
+
+# 2.
+
+x = 2*1.5
+
+print(f" THE ANSWER OF MULTIPLY IS = {x}")
+
+# 3.
+
+y = 4 - 1.5
+
+print(f" THE ANSWER OF MINUS IS = {y}")
+
+# 4.
+
+z = 10 / 2
+
+print(f" THE ANSWER OF DIVISION IS = {z}")
---
+++ Data_Type/example/example1.py
@@ -0,0 +1,62 @@
+# 1 - string example :
+
+print("jay rote")
+
+# -----------------------
+
+a = "jay" + "rote"
+print(a)
+
+# 2 - list example :
+
+#YOU ADD ANY DATATYPE IN LIST!
+
+l = [ 1 , 2 , 3 , "jay" , "hello" , 1.10 , 20 ]
+
+print(l)
+
+# 3 - integer
+
+x = 10
+
+y = 15
+
+z = x + y
+
+print(z)
+
+# 4 - float
+
+p = 1.50 + 2.50
+
+print(p)
+
+# 5 - dictionary
+
+dictionary = { "name" : "jay" , "age" : 18 , "school" : "SSVM" }
+
+print(dictionary)
+
+# ----------------------------------
+
+i = dictionary["name"]
+
+print(i)
+
+# 6 - tuple
+
+t = ("jay" , 2 , "rote" , 1.12)
+
+print(t)
+
+# 7 - bool
+
+name = "jay"
+
+if name == "jay":
+
+    True
+
+else:
+
+    False
